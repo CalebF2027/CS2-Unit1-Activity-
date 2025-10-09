a6f30ccc2be48642e6aa2ec6599e8352558dff31
caleb <caleb.fisher2027@gbwl.org>
2025-10-09 17:47:29 +0000

    fijished input

--- Main.java
+++ Main.java
@@ -7,34 +7,34 @@ public class Main {
       System.out.println("Let's play MadLibs!");
 
     // 2. Set up a Scanner OBJECT
-      Scanner scan = new Scanner(System.in);
+      Scanner input = new Scanner(System.in);
       // System.in refers to the physical keyboard!
 
       // 3. Use the Scanner to take input
       System.out.println("Type your name then press enter: ");
-      String player = scan.nextLine();
+      String player = input.nextLine();
       System.out.println("Welcome, " + player);
 
-     String adjective1 = "angry";
-     System.out.print("Enter an adjective: ");
-      String place1 = "school";
-      System.out.print("Enter a place: ");
-      String noun1 = "plant";
-      System.out.print("Enter a noun: ");
-      String verb1 = "jump";
-      System.out.print("Enter a verb: ");
-      String adverb1 = "quickly";
-      System.out.print("Enter an adverb: ");
-      String adjective2 = "happy";
       System.out.print("Enter an adjective: ");
-      String place2 = "house";
-      System.out.print("Enter a place: ");
-      String noun2 = "phone";
-      System.out.ptint("Enter a noun: ");
-      String adverb2 = "yesterday";
-      System.out.print("Enter an adverb: ");
-      String verb2 = "run";
-      System.out.print("Enter a verb: ");
+     String adjective1 = input.nextLine();
+       System.out.print("Enter a place: ");
+      String place1 = input.nextLine();
+       System.out.print("Enter a noun: ");
+      String noun1 = input.nextLine();
+       System.out.print("Enter a verb: ");
+      String verb1 = input.nextLine();
+       System.out.print("Enter an adverb: ");
+      String adverb1 = input.nextLine();
+        System.out.print("Enter an adjective: ");
+      String adjective2 = input.nextLine();
+           System.out.print("Enter a place: ");
+      String place2 = input.nextLine();
+        System.out.print("Enter a noun: ");
+      String noun2 = input.nextLine();
+        System.out.print("Enter an adverb: ");
+      String adverb2 = input.nextLine();
+        System.out.print("Enter a verb: ");
+      String verb2 = input.nextLine();
 
       String sentence1 = " Today I was " + adjective1 + " because I had to go to " + place1 + " I saw a " + noun1 + " I was scared so I deicded to " + verb1 + " I ran to school " + adverb1 + " I was not " + adjective2 + " After school I went to my " + place2 + " I scrolled on my " + noun2 + " I did my homework from " + adverb2 + " I went on a " + verb2;
 
